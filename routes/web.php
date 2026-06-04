2af6e137b12b327e8801d7dba3f0c70ead88c63c
LohaneSthefany-2 <Loohane456@outlook.com>
2026-06-04 16:09:10 -0300

    mudei mais coisa do login, e mais tarde eu faço mais dele

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
+Route::get('/', [LoginController::class, 'index']);
+
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'autenticar']);
+
 Route::get('/logout', [LoginController::class, 'logout']);
 
 Route::get('/painel', function () {
